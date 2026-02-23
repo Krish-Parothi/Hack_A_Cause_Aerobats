5a8bd8759e6ee00ea144d7cdf084ddb09e490502
Krish-Parothi <krrishparothi10@gmail.com>
2026-02-23 11:57:20 +0530

    Updated the model with more accuracy

---
+++ testing/Single_Image_test.py
@@ -0,0 +1,23 @@
+# from ultralytics import YOLO
+
+# model = YOLO("Model/runs/yolov8_custom4/weights/best.pt")
+
+# results = model("testing/test_images/test.jpg", conf=0.3, save=True)
+
+# print(results)
+from ultralytics import YOLO
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+model = YOLO(os.path.join(BASE_DIR, "Model", "runs", "yolov8_custom4", "weights", "best.pt"))
+
+results = model(
+    os.path.join(BASE_DIR, "testing", "test_images", "test.jpg"),
+    conf=0.3,
+    save=True,
+    project=os.path.join(BASE_DIR, "testing", "output"),  # save folder
+    name="results"  # subfolder name
+)
+
+print(results)
